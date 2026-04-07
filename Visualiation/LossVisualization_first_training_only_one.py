33799dfeb9059e055a7082f8e1b1f9d60ee5b6a0
F10rian <florian.fingscheidt@gmx.de>
2026-04-07 23:22:52 +0200

    Monir changes

---
+++ Visualiation/LossVisualization_first_training_only_one.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Read the loss data
+loss_data = pd.read_csv('../normal_training/loss.csv')
+
+#Plot the training and validation loss
+#plt.figure(figsize=(10, 6))
+plt.plot(loss_data['epoch'], loss_data['train_loss'], label='Training Loss')
+plt.axline((0, 2.3), slope=0, color='red', label = 'Baseline')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training and Validation Loss Over Epochs')
+plt.legend()
+plt.grid(True)
+plt.savefig('loss_plot.png')
+
+plt.savefig('first_training_only_one.svg')
+plt.show()
